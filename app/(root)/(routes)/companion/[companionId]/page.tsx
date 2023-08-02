288ccab576b8b5ca0ade49b2936aaddbeed9e59c
zak <mzhussain092@gmail.com>
2023-08-02 16:14:19 +0530

    add CompanionForm

---
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -0,0 +1,24 @@
+﻿import prismadb from "@/lib/prismadb";
+import React from "react";
+import CompanionForm from "./components/CompanionForm";
+
+interface CompanionPageProps {
+  params: {
+    companionId: string;
+  };
+}
+
+const CompanionPage = async ({ params }: CompanionPageProps) => {
+  // TODO:Check Subscription
+
+  const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    },
+  });
+
+  const categories = await prismadb.category.findMany();
+  return <CompanionForm initialData={companion} categories={categories} />;
+};
+
+export default CompanionPage;
