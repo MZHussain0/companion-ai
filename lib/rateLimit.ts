ca890b64b04cd7004619a2547f82f24ec3ddc4a8
zak <mzhussain092@gmail.com>
2023-08-04 15:45:10 +0530

    add api/chat POST endpoint

---
+++ lib/rateLimit.ts
@@ -0,0 +1,13 @@
+﻿import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
+export async function ratelimit(identifier: string) {
+  const rateLimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(10, "10 s"),
+    analytics: true,
+    prefix: "@upstash/ratelimit",
+  });
+
+  return await rateLimit.limit(identifier);
+}
