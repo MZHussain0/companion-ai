47c54c563f1392083566d1cf0792f22dd198e53b
zak <mzhussain092@gmail.com>
2023-08-27 14:34:12 +0530

    fixtype error in sidebar

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -3,16 +3,18 @@ import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Menu } from "lucide-react";
 import Sidebar from "./Sidebar";
 
-interface MobileSidebarProps {}
+interface MobileSidebarProps {
+  isPro: boolean;
+}
 
-const MobileSidebar: FC<MobileSidebarProps> = ({}) => {
+const MobileSidebar: FC<MobileSidebarProps> = ({ isPro }) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
